06bc547ae30f8c6c6fb19ebc25e1f38660d1797e
Rhett North <retsrawk@gmail.com>
2024-03-17 01:01:17 -0600

    Setup, HTML skeleton
    In this commit, I have undergone the setup process
    for this repository, including a file structure,
    now with consideration for usage of webpack. I
    have also set up an HTML skeleton to lay the
    foundation of my project. In further commits, most
    of the rest of the content will be set up using js
    with CSS styling.

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+    entry: "./src/index.js",
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: "Restaurant Page",
+            template: path.resolve(__restaurant-page, "./src/index.html"),
+            filename: "index.html",
+        }),
+    ],
+    output: {
+        filename: "main.js",
+        path: path.resolve(__dirname, "dist"),
+        clean: true,
+    },
+};
