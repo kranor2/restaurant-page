1c10aa222cd667fe4545829797b1b814a6469b6c
Rhett North <retsrawk@gmail.com>
2024-03-21 10:26:35 -0600

    Create nav socials in js
    In this commit, I have re-written the code
    creating my socials icons within the nav bar in
    JS using DOM manipulation techniques. The images
    still will not render, which I am actively
    troubleshooting, but they are now adequately
    imported from src to dist.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,46 @@
 import "./style.css";
 import Background from "./koi-fish_bkgd.gif";
+import InstaIcon from "./instagram-icon.png";
+import TwitchIcon from "./twitch-icon.png";
+import YoutubeIcon from "./youtube-icon.png";
+import LinkedinIcon from "./linkedin-icon.png";
 
+const Nav = document.querySelector("nav");
+const Socials = document.createElement("div");
+Socials.classList.add("socials");
+
+const Insta = document.createElement("a");
+Insta.href = "https://www.instagram.com/goodappleboy/";
+Insta.classList.add("icon");
+const instaLink = document.createElement("img");
+instaLink.src = {InstaIcon};
+Insta.appendChild(instaLink);
+Socials.appendChild(Insta);
+
+const Twitch = document.createElement("a");
+Twitch.href = "https://www.twitch.tv/goodappleboy";
+Twitch.classList.add("icon");
+const twitchLink = document.createElement("img");
+twitchLink.src = {TwitchIcon};
+Twitch.appendChild(twitchLink);
+Socials.appendChild(Twitch);
+
+const Youtube = document.createElement("a");
+Youtube.href = "https://www.youtube.com/@goodappleboy";
+Youtube.classList.add("icon");
+const youtubeLink = document.createElement("img");
+youtubeLink.src = {YoutubeIcon};
+Youtube.appendChild(youtubeLink);
+Socials.appendChild(Youtube);
+
+const Linkedin = document.createElement("a");
+Linkedin.href = "https://www.linkedin.com/in/rdcnorth";
+Linkedin.classList.add("icon");
+const linkedinLink = document.createElement("img");
+linkedinLink.src = {LinkedinIcon};
+Linkedin.appendChild(linkedinLink);
+Socials.appendChild(Linkedin);
+
+Nav.appendChild(Socials);
 
 console.log("webpack did the thing");
