31f6accae70954036fce706ca1c0a92167a0ecdf
Rhett North <retsrawk@gmail.com>
2024-03-21 10:55:26 -0600

    Fix syntax error re: socials icons
    In this commit, I fixed minor syntactical errors
    on the lines sourcing the images for the socials
    icons in the nav bar. I also applied the icon
    class to each of these images, as well as their
    link elements, in order to properly size the icons
    within the nav bar.

--- src/index.js
+++ src/index.js
@@ -13,7 +13,8 @@ const Insta = document.createElement("a");
 Insta.href = "https://www.instagram.com/goodappleboy/";
 Insta.classList.add("icon");
 const instaLink = document.createElement("img");
-instaLink.src = {InstaIcon};
+instaLink.src = InstaIcon;
+instaLink.classList.add("icon");
 Insta.appendChild(instaLink);
 Socials.appendChild(Insta);
 
@@ -21,7 +22,8 @@ const Twitch = document.createElement("a");
 Twitch.href = "https://www.twitch.tv/goodappleboy";
 Twitch.classList.add("icon");
 const twitchLink = document.createElement("img");
-twitchLink.src = {TwitchIcon};
+twitchLink.src = TwitchIcon;
+twitchLink.classList.add("icon");
 Twitch.appendChild(twitchLink);
 Socials.appendChild(Twitch);
 
@@ -29,7 +31,8 @@ const Youtube = document.createElement("a");
 Youtube.href = "https://www.youtube.com/@goodappleboy";
 Youtube.classList.add("icon");
 const youtubeLink = document.createElement("img");
-youtubeLink.src = {YoutubeIcon};
+youtubeLink.src = YoutubeIcon;
+youtubeLink.classList.add("icon");
 Youtube.appendChild(youtubeLink);
 Socials.appendChild(Youtube);
 
@@ -37,7 +40,8 @@ const Linkedin = document.createElement("a");
 Linkedin.href = "https://www.linkedin.com/in/rdcnorth";
 Linkedin.classList.add("icon");
 const linkedinLink = document.createElement("img");
-linkedinLink.src = {LinkedinIcon};
+linkedinLink.src = LinkedinIcon;
+linkedinLink.classList.add("icon");
 Linkedin.appendChild(linkedinLink);
 Socials.appendChild(Linkedin);
 
